361af102dd0be86b1b491e4e7bf91bfb6bb6e5bb
l4 <lewisculleton00@gmail.com>
2024-11-15 13:06:39 +0000

    Added cloudfront distrobution for caching

---
+++ cloudfront.tf
@@ -0,0 +1,52 @@
+locals {
+  s3_origin_id = "porfolio-s3"
+}
+
+resource "aws_cloudfront_distribution" "s3_disribution" {
+
+  origin {
+    domain_name = aws_s3_bucket.portfolio.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id = local.s3_origin_id
+  }
+
+  enabled = true
+  default_root_object = "index.html"
+  
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "portfolio"
+  description                       = "portfolio access control polciy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
   bucket_name = "l4-portfolio-bucket" 
+  region = "eu-west-1"
 }
