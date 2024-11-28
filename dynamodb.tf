355b989706c0c254466ed1a87f3b9dbb3a678d82
l4 <lewisculleton00@gmail.com>
2024-11-28 18:30:02 +0000

    typo

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "visitors" {
   name             = "visitors"
   hash_key         = "visitors_table_hash_key"
-  billing_mode     = "PAY_PER_REQUET"
+  billing_mode     = "PAY_PER_REQUSET"
   stream_enabled   = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
 
