f48041193b01903012f1d6906924f552ce0b1ecd
l4 <lewisculleton00@gmail.com>
2024-11-28 18:29:05 +0000

    added dynamo db

---
+++ dynamodb.tf
@@ -0,0 +1,15 @@
+resource "aws_dynamodb_table" "visitors" {
+  name             = "visitors"
+  hash_key         = "visitors_table_hash_key"
+  billing_mode     = "PAY_PER_REQUET"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  replica {
+    region_name = "eu-west-1"
+  }
+
+  replica {
+    region_name = "eu-west-1"
+  }
+}
